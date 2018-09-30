f1fbda584780081ce7b899c18ac657a95eb2b9d1
Melevyse <38793426+Melevyse@users.noreply.github.com>
2018-09-30 18:42:48 +0300

    Add files via upload

---
+++ woork1/src/com/company/Book.java
@@ -0,0 +1,55 @@
+package com.company;
+
+public class Book {
+    private String color;
+    private int countPage;
+    private String name;
+    public Book()
+    {
+        color = "";
+        countPage = 0;
+        name = "";
+    }
+    public Book(String line)
+    {
+        color = line;
+        countPage = 0;
+        name = "";
+    }
+    public Book(String line, int count)
+    {
+        color = line;
+        countPage = count;
+        name = "";
+    }
+    public Book(String line, int count, String str)
+    {
+        color = line;
+        countPage = count;
+        name = str;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getCountPage() {
+        return countPage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setCountPage(int countPage) {
+        this.countPage = countPage;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

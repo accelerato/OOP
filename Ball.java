cacd5f339594cee9ac7d190242e6d1c7e9d96fa9
Melevyse <38793426+Melevyse@users.noreply.github.com>
2018-09-30 17:34:10 +0300

    Create Ball.java

---
+++ Ball.java
@@ -0,0 +1,55 @@
+package com.company;
+
+public class Ball {
+    private String color;
+    private int weight;
+    private int radius;
+    public Ball()
+    {
+        color = "";
+        radius = 0;
+        weight = 0;
+    }
+    public Ball(int wT)
+    {
+        weight = wT;
+        radius = 0;
+        color = "";
+    }
+    public Ball(int wT, int r)
+    {
+        weight = wT;
+        radius = r;
+        color = "";
+    }
+    public Ball(int wT, int r, String line)
+    {
+        weight = wT;
+        radius = r;
+        color = line;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}

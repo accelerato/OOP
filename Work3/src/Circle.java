9f9a32a446514d8ce584679d590356cea2c70539
Melevyse <38793426+Melevyse@users.noreply.github.com>
2018-09-30 18:43:11 +0300

    Add files via upload

---
+++ Work3/src/Circle.java
@@ -0,0 +1,41 @@
+public class Circle extends Shape{
+    protected double radius;
+    public Circle()
+    {
+        radius = 0;
+        this.color = "";
+        this.filled = false;
+    }
+    public Circle(double radius)
+    {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+    public Circle(double radius, String color, boolean filled)
+    {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    @Override
+    public double getArea() {
+        return Math.PI*radius*radius;
+    }
+    @Override
+    public double getPerimeter() {
+        return 2*Math.PI*radius;
+    }
+    @Override
+    public String toString() {
+        return "Shape: circle, radius: "+this.radius+", color: "+this.color;
+    }
+}
---
+++ Work3/src/Rectangle.java
@@ -0,0 +1,53 @@
+public class Rectangle extends Shape{
+    protected double width;
+    protected double length;
+    public Rectangle()
+    {
+        width = 0;
+        length = 0;
+        this.color = "";
+        this.filled = false;
+    }
+    public Rectangle(double width, double length)
+    {
+        this.width = width;
+        this.length = length;
+        this.color = "";
+        this.filled = false;
+    }
+    public Rectangle(double width, double length, String color, boolean filled)
+    {
+        this.width = width;
+        this.length = length;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+    @Override
+    public double getArea() {
+        return width*length;
+    }
+    @Override
+    public double getPerimeter() {
+        return 2*(width+length);
+    }
+    @Override
+    public String toString() {
+        return "Shape: width: "+this.width +" length: "+this.length+", color: "+this.color;
+    }
+}

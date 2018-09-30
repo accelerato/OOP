30bf2d6ce2b0dd65814d5f52ce7beb3e1b0b9a01
Melevyse <38793426+Melevyse@users.noreply.github.com>
2018-09-30 18:42:26 +0300

    Add files via upload

---
+++ Laba3/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Tarelka t =	new Tarelka("Керамическая","Белый","Большая");
+                System.out.println(t);
+    }
+}
